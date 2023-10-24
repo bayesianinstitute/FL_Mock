7b71648e81edcf920819c2ba82eb39bc61003351
afaan <afaan@bayes.global>
2023-10-24 09:26:01 -0700

    with tensor tensorflow

--- ipfs.py
+++ ipfs.py
@@ -1,94 +1,76 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
 import os
 import ipfshttpclient
 import io
+import tensorflow as tf
+from tensorflow import keras
 
-# Define a simple model
-class SimpleModel(nn.Module):
-    def __init__(self):
-        super(SimpleModel, self).__init__()
-        self.fc = nn.Linear(10, 1)
+# Define a simple model using Keras
+model = keras.Sequential([
+    keras.layers.Dense(1, input_shape=(10,))
+])
 
-    def forward(self, x):
-        return self.fc(x)
-    
-def train_model(worker_id):
-    model = SimpleModel()
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
-    criterion = nn.MSELoss()
+model.compile(optimizer='sgd', loss='mean_squared_error')
 
-    input_data = torch.randn(100, 10)
-    target = torch.randn(100, 1)
+# Generate some random data for training
+input_data = tf.random.normal((100, 10))
+target = tf.random.normal((100, 1))
 
-    epochs = 100
-    for epoch in range(epochs):
-        optimizer.zero_grad()
-        output = model(input_data)
-        loss = criterion(output, target)
-        loss.backward()
-        optimizer.step()
+# Train the model
+model.fit(input_data, target, epochs=100)
 
-    # Save the trained model using torch.jit.script
-    scripted_model = torch.jit.script(model)
-    torch.jit.save(scripted_model, f'trained_model_{worker_id}.pt')
-    return 'trained_model.pt'
+# Save the trained model using Keras's format
+model.save('saved_model.h5')
 
-# IPFS communication class
+# IPFS communication class (similar to the PyTorch version)
 class IPFS:
-    def __init__(self, ipfs_path, device):
+    def __init__(self, ipfs_path):
         self.ipfs_path = ipfs_path
-        self.DEVICE = device
         self.client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001/http')
 
     def fetch_model(self, model_hash):
         model_bytes = self.client.cat(model_hash)
-        model = torch.jit.load(io.BytesIO(model_bytes), map_location=self.DEVICE)
+        model = keras.models.load_model(io.BytesIO(model_bytes))
         return model
 
-    def push_model(self, saved_model_pb_path):
-        model_filename = saved_model_pb_path
-        model_hash = self.client.add(model_filename)['Hash']
+    def push_model(self, saved_model_path):
+        model_hash = self.client.add(saved_model_path)['Hash']
         return model_hash
 
     def download_model(self, model_hash, destination_folder):
         model_bytes = self.client.cat(model_hash)
-        worker_id=3
-        # Specify the path for saving the model file locally
-        local_model_path = os.path.join(destination_folder, f'model_{worker_id}.pt')
 
-        # Save the downloaded model to the local path
+        # Specify the path for saving the model file locally
+        local_model_path = os.path.join(destination_folder, 'model.h5')
+
         with open(local_model_path, 'wb') as f:
             f.write(model_bytes)
 
         # Load the model from the local path
-        model = torch.jit.load(local_model_path, map_location=self.DEVICE)
-        
+        model = keras.models.load_model(local_model_path)
         return model
 
 if __name__ == "__main__":
     ipfs_path = '/your/ipfs/path'
-    device = 'cpu'
 
-    ipfs_communicator = IPFS(ipfs_path, device)
+    ipfs_communicator = IPFS(ipfs_path)
 
-    # Train the model and get the model file path
-    trained_model_path = train_model()
-
-    # Push the trained model to IPFS and get the hash
-    model_hash = ipfs_communicator.push_model(trained_model_path)
-
-    # Access the model hash
-    print("Model Hash:", model_hash)
 
     # Define the destination folder to save the downloaded model
     destination_folder = 'downloaded_models'
 
+
+    # Push the trained model to IPFS and get the hash
+    model_hash = ipfs_communicator.push_model('saved_model.h5')
+
+    # Access the model hash
+    print("Model Hash:", model_hash)
+
     # Download the model and save it to the destination folder
     downloaded_model = ipfs_communicator.download_model(model_hash, destination_folder)
 
-    # # You can now use the downloaded model for inference
-    # input_data = torch.randn(1, 10)  # Replace with your input data
-    # output = downloaded_model(input_data)
-    # print("Inference result:", output)
+    print("Model downloaded:", downloaded_model)
+
+
+    # model=ipfs_communicator.fetch_model(model_hash)
+
+    # model.summary()
