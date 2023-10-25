bf5f6cfa2a34094466fed3bec728042b7be07aae
faizack <faizack619@gmail.com>
2023-10-25 22:18:37 +0530

    add qos as 1

--- test/identify/testidentify.py
+++ test/identify/testidentify.py
@@ -5,6 +5,13 @@ import json
 import random
 import psutil  # Import the psutil library
 
+import paho.mqtt.client as mqtt
+import time
+import subprocess
+import json
+import random
+import psutil  # Import the psutil library
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe("ram_topic")
@@ -58,7 +65,55 @@ def main():
 
     while True:
         shared_count = client._userdata["shared_count"]
-        if shared_count < 3:
+        if shared_count < 2:
+            time.sleep(10)  # Wait for more clients to share data
+        else:
+            # Check if this node has the highest RAM usage
+            ram_usages = client._userdata["ram_usages"]
+            if is_highest_ram_usage(ram_usage, ram_usages):
+                declare_aggregator(client, participant_id)
+                print(f"I am the aggregator! RAM usage: {ram_usage} MB")
+                break
+
+def is_highest_ram_usage(current_ram_usage, ram_usages):
+    # Check if current_ram_usage is equal to or higher than the highest RAM usage
+    max_ram_usage = max(ram_usages.values())
+    return current_ram_usage >= max_ram_usage
+
+if __name__ == '__main__':
+    main()
+
+
+def measure_ram_usage():
+    try:
+        ram_usage = psutil.virtual_memory().used / (1024 ** 2)  # Get RAM usage in MB
+        print("ram_usage : {ram_usage} MB",ram_usage)
+        return ram_usage
+    except Exception as e:
+        print(f"Error measuring RAM usage: {str(e)}")
+    return 0  # Default to 0 if measurement fails
+
+def main():
+    broker = 'test.mosquitto.org'
+    participant_id = f"Machine-id- {random.randint(1, 1000)}"  # Unique identifier for each machine
+
+    print("my id ",participant_id)
+
+    client = mqtt.Client(userdata={"ram_usages": {}, "shared_count": 0})
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect(broker, 1883)
+    client.loop_start()
+
+    # Measure the node's RAM usage
+    ram_usage = measure_ram_usage()
+
+    # Announce RAM usage
+    announce_ram_usage(client, participant_id, ram_usage)
+
+    while True:
+        shared_count = client._userdata["shared_count"]
+        if shared_count < 2:
             time.sleep(10)  # Wait for more clients to share data
         else:
             # Check if this node has the highest RAM usage
