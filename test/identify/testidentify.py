58b5c34f574cc23eec79b85a39d3f9e9a6ef05bb
faizack <faizack619@gmail.com>
2023-10-25 22:59:30 +0530

    checking iden

--- test/identify/testidentify.py
+++ test/identify/testidentify.py
@@ -1,16 +1,8 @@
 import paho.mqtt.client as mqtt
 import time
-import subprocess
 import json
 import random
-import psutil  # Import the psutil library
-
-import paho.mqtt.client as mqtt
-import time
-import subprocess
-import json
-import random
-import psutil  # Import the psutil library
+import psutil
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
@@ -21,8 +13,8 @@ def on_message(client, userdata, message):
     node_id = ram_info["node_id"]
     ram_usage = ram_info["ram_usage"]
     print(f"Received RAM usage from {node_id}: {ram_usage} MB")
-    userdata[node_id] = ram_usage
-    userdata["shared_count"] += 1  # Increment the shared count
+    userdata["ram_usages"][node_id] = ram_usage
+    userdata["shared_count"] += 1
 
 def announce_ram_usage(client, participant_id, ram_usage):
     topic = 'ram_topic'
@@ -40,7 +32,7 @@ def declare_aggregator(client, participant_id):
 def measure_ram_usage():
     try:
         ram_usage = psutil.virtual_memory().used / (1024 ** 2)  # Get RAM usage in MB
-        print("ram_usage : {ram_usage} MB",ram_usage)
+        print(f"ram_usage: {ram_usage} MB")
         return ram_usage
     except Exception as e:
         print(f"Error measuring RAM usage: {str(e)}")
@@ -48,62 +40,9 @@ def measure_ram_usage():
 
 def main():
     broker = 'test.mosquitto.org'
-    participant_id = f"Machine-id- {random.randint(1, 1000)}"  # Unique identifier for each machine
+    participant_id = f"Machine-id-{random.randint(1, 1000)}"
 
-    print("my id ",participant_id)
-
-    client = mqtt.Client(client_id=participant_id,userdata={"ram_usages": {}, "shared_count": 0},clean_session=False)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(broker, 1883)
-    client.loop_start()
-
-    time.sleep(15)
-
-
-    # Measure the node's RAM usage
-    ram_usage = measure_ram_usage()
-
-    # Announce RAM usage
-    announce_ram_usage(client, participant_id, ram_usage)
-
-    while True:
-        
-        shared_count = client._userdata["shared_count"]
-        if shared_count < 2:
-            print("Wait for more clients to share data shared count :",shared_count)
-            time.sleep(10)  # Wait for more clients to share data
-        else:
-            # Check if this node has the highest RAM usage
-            ram_usages = client._userdata["ram_usages"]
-            if is_highest_ram_usage(ram_usage, ram_usages):
-                declare_aggregator(client, participant_id)
-                print(f"I am the aggregator! RAM usage: {ram_usage} MB")
-                break
-
-def is_highest_ram_usage(current_ram_usage, ram_usages):
-    # Check if current_ram_usage is equal to or higher than the highest RAM usage
-    max_ram_usage = max(ram_usages.values())
-    return current_ram_usage >= max_ram_usage
-
-if __name__ == '__main__':
-    main()
-
-
-def measure_ram_usage():
-    try:
-        ram_usage = psutil.virtual_memory().used / (1024 ** 2)  # Get RAM usage in MB
-        print("ram_usage : {ram_usage} MB",ram_usage)
-        return ram_usage
-    except Exception as e:
-        print(f"Error measuring RAM usage: {str(e)}")
-    return 0  # Default to 0 if measurement fails
-
-def main():
-    broker = 'test.mosquitto.org'
-    participant_id = f"Machine-id- {random.randint(1, 1000)}"  # Unique identifier for each machine
-
-    print("my id ",participant_id)
+    print("My ID:", participant_id)
 
     client = mqtt.Client(userdata={"ram_usages": {}, "shared_count": 0})
     client.on_connect = on_connect
@@ -128,6 +67,8 @@ def main():
                 declare_aggregator(client, participant_id)
                 print(f"I am the aggregator! RAM usage: {ram_usage} MB")
                 break
+            else :
+                print("I am not the aggregator")
 
 def is_highest_ram_usage(current_ram_usage, ram_usages):
     # Check if current_ram_usage is equal to or higher than the highest RAM usage
