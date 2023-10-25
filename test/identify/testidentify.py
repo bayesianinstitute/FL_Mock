6ad8b229badab580c05bc136f9d0f64740cbf729
afaan <afaan@bayes.global>
2023-10-25 11:45:24 -0700

    added retaine to identifyparticipant

--- test/identify/testidentify.py
+++ test/identify/testidentify.py
@@ -33,7 +33,7 @@ class IdentifyParticipant:
             "node_id": self.participant_id,
             "ram_usage": ram_usage
         }
-        self.client.publish(topic, json.dumps(ram_info))
+        self.client.publish(topic, json.dumps(ram_info), qos=1, retain=True)
 
     def declare_aggregator(self):
         topic = 'aggregator_topic'
