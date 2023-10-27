25be3766426d89b6adb085afb02d00cc83d6ca8c
faizack <faizack619@gmail.com>
2023-10-28 01:54:46 +0530

    adding test

--- testt/identify/testidentify.py
+++ testt/identify/testidentify.py
@@ -4,41 +4,29 @@ import json
 import psutil
 
 class IdentifyParticipant:
-    def __init__(self, id,broker='test.mosquitto.org',):
-        self.broker = broker
+    def __init__(self, id,):
+        self.broker = 	'broker.hivemq.com'
         self.id=id
         self.computer_info = self.get_computer_info()
         self.participant_id = f"Machine-id-{self.id}"
         self.client = mqtt.Client(client_id=self.participant_id, userdata={"ram_usages": {}, "shared_count": 0})
-        # self.client.on_connect = self.on_connect
+        self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
         # self.client.on_disconnect = self.on_disconnect
         self.client.connect(self.broker, 1883)
-        self.client.subscribe("ram_topic")
         self.client.loop_start()
         self.aggregator = False  # Initialize as non-aggregator
 
     def get_computer_info(self):
         # total_ram = psutil.virtual_memory().total / (1024 ** 3)  # RAM in GB
-        # Get RAM usage
-        ram = psutil.virtual_memory()
-        used_ram = ram.used
-        total_ram = ram.total
+        return {"ram": self.id}
 
-        # Get available RAM
-        available_ram = ram.available
-
-        print(f"Used RAM: {used_ram / (1024 ** 3):.2f} GB")
-        print(f"Total RAM: {total_ram / (1024 ** 3):.2f} GB")
-        print(f"Available RAM: {available_ram / (1024 ** 3):.2f} GB")
-        return {"ram": available_ram}
-
-    # def on_connect(self, client, userdata, flags, rc):
-    #     if rc == 0:
-    #         print(f"Connected to Broker")
-    #         client.subscribe("ram_topic")
-    #     else:
-    #         print("Unable to connect to Broker result code: {}".format(rc))
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print(f"Connected to Broker")
+            client.subscribe("ram_topic")
+        else:
+            print("Unable to connect to Broker result code: {}".format(rc))
 
     def on_message(self, client, userdata, message):
         ram_info = json.loads(message.payload.decode("utf-8"))
@@ -61,7 +49,7 @@ class IdentifyParticipant:
     def declare_aggregator(self):
         topic = 'aggregator_topic'
         aggregator_message = f"Machine {self.participant_id} is the aggregator!"
-
+        # time.sleep(10)
         self.client.publish(topic, aggregator_message)
 
     def is_highest_ram_usage(self, current_ram_usage):
@@ -91,6 +79,7 @@ class IdentifyParticipant:
                 else:
                     print("I am not the aggregator")
                     self.client.disconnect()
+
                     return self.aggregator
 
             # time.sleep(10)  # Add a delay to avoid constantly checking
