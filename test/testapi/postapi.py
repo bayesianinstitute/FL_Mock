d0ab0baccc7281156da5f6b5b64f1bdf7d0d6605
Afaan <afaan@bayes.global>
2023-11-30 01:14:05 +0530

    post api test

---
+++ test/testapi/postapi.py
@@ -0,0 +1,24 @@
+import requests
+
+url = "http://127.0.0.1:8000/api/v1/create_training_result/"
+
+data = {
+    "training_accuracy": 1,
+    "validation_accuracy": 1,
+    "loss": 1,
+    "training_info": 1
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+# Sending a POST request to the API endpoint
+response = requests.post(url, json=data, headers=headers)
+
+# Check the response
+if response.status_code == 201:  # Assuming 201 means successful creation
+    print("Data added successfully.")
+else:
+    print(f"Failed to add data. Status code: {response.status_code}")
+    print(response.text)
