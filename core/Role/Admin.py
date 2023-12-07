e9224ecc3779b0d75c280a17b17657f55bc35040
Afaan <afaan@bayes.global>
2023-12-07 23:42:48 +0530

    error handling in roles

--- core/Role/Admin.py
+++ core/Role/Admin.py
@@ -17,180 +17,180 @@ class AdminOPS:
     
 
     def admin_logic(self):
-        self.logger.info("I am Admin ")
-        
-        id=self.mqtt_obj.receive_msg(role='Admin')
-        self.mqtt_obj.send_id(role='Admin',id=id)
-        while True:
-            self.logger.debug("Waiting")
-            time.sleep(9)
-        #TODO: if the user sending message using mqtt add user in database and also updates connected status
-    
-        admin_data = {
-            "training_status": "in_progress",
-            "role": "Admin",
-            "node_id": 8,
-            "model_hash": "your_model_hash_here",
-            "network_status": "connected"
-        }
-   
-        status=self.add_admin(admin_data)
-        if status:
-            self.logger.info("Added")
-        else:
-            self.logger.error("Not Added")
-            
-        #TODO: Check no_of_user in database
-            # if the user is 1 do not aggregate and update the global model hash 
-            # if user is more than 2 wait for their all their model hash then aggregation 
+        try:
+            self.logger.info("I am Admin ")
 
-        user_count = self.get_node_count()
+            id = self.mqtt_obj.receive_msg(role='Admin')
+            self.mqtt_obj.send_id(role='Admin', id=id)
+            while True:
+                self.logger.debug("Waiting")
+                time.sleep(9)
 
-        # Check if the user count is 1 (no aggregation needed) or more than 2 (wait for all hashes)
-        if user_count == 1:
-            self.logger.debug("User count: 1")
-            data = {
-                "global_model_hash": "HAsha256",
-            }
-            #user Train model hash 
-            
-            
-            # If only one user, update the global model hash immediately
-            # Api to update the global model
-            status=self.add_global_model_hash(data)
-            if status:
-                self.logger.debug("Added global model")
-            else:
-                self.logger.error("Not Added global model")
-            #self.global_model_hash = user_model_hash
-        elif user_count > 2:
-            # If more than two users, wait for all users to submit their model hashes
-            #self.user_model_hashes.add(user_model_hash)
-            
-            # Api to update the global model
-            self.logger.debug("More than User count: 2")
-            data = {
-                "global_model_hash": "HAsha256",
+            admin_data = {
+                "training_status": "in_progress",
+                "role": "Admin",
+                "node_id": 8,
+                "model_hash": "your_model_hash_here",
+                "network_status": "connected"
             }
 
-            #TODO: perform Aggregation
-            
-            
-            # If only one user, update the global model hash immediately
-            # Api to update the global model
-            status=self.add_global_model_hash(data)
+            status = self.add_admin(admin_data)
             if status:
-                self.logger.debug("Added global model")
+                self.logger.info("Added")
             else:
-                self.logger.error("Not Added global model")
-            # if len(self.user_model_hashes) == user_count:
-            #     # Perform aggregation when all users have submitted their model hashes
-            #     self.global_model_hash = self.perform_aggregation(self.user_model_hashes)
-            #     self.aggregation_in_progress = False
+                self.logger.error("Not Added")
+
+            user_count = self.get_node_count()
+
+            if user_count == 1:
+                self.logger.debug("User count: 1")
+                data = {
+                    "global_model_hash": "HAsha256",
+                }
+
+                status = self.add_global_model_hash(data)
+                if status:
+                    self.logger.debug("Added global model")
+                else:
+                    self.logger.error("Not Added global model")
+            elif user_count > 2:
+                self.logger.debug("More than User count: 2")
+                data = {
+                    "global_model_hash": "HAsha256",
+                }
+
+                status = self.add_global_model_hash(data)
+                if status:
+                    self.logger.debug("Added global model")
+                else:
+                    self.logger.error("Not Added global model")
+
+        except Exception as e:
+            self.logger.error(f"Error in admin_logic: {str(e)}")               
 
 
            
 
     def add_admin(self, admin_data):
-        response = self.apiClient.post_request(add_nodes, admin_data)
+        try:
+            response = self.apiClient.post_request(add_nodes, admin_data)
 
-        if response and response.status_code == 201:
-            self.logger.info(f"POST add_admin Request Successful: {response.text}")
-            return json.loads(response.text)
-        else:
-            self.logger.info(f"POST Request Failed: {response.status_code, response.text}")
+            if response and response.status_code == 201:
+                self.logger.info(f"POST add_admin Request Successful: {response.text}")
+                return json.loads(response.text)
+            else:
+                self.logger.error(f"POST Request Failed: {response.status_code, response.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in add_admin: {str(e)}")
             return None
     
     def get_node_count(self):
+        try:
+            response = self.apiClient.get_request(node_id_count)
 
-        # Make the GET request using the get_request method
-        response = self.apiClient.get_request(node_id_count)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response and get the user count
-            count_data = response.json()
-            user_count = count_data.get('count')
-            return user_count
-        else:
-            # Handle the case where the request was not successful
-            print(f"GET Request Failed: {response.status_code}, {response.text}")
+            if response.status_code == 200:
+                count_data = response.json()
+                user_count = count_data.get('count')
+                return user_count
+            else:
+                print(f"GET Request Failed: {response.status_code}, {response.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in get_node_count: {str(e)}")
             return None
     
     def add_global_model_hash(self, data):
-        response = self.apiClient.post_request(add_global_model_hash, data)
+        try:
+            response = self.apiClient.post_request(add_global_model_hash, data)
 
-        if response and response.status_code == 200:
-            self.logger.info(f"POST add_global_model_hash Request Successful: {response.text}")
-            return json.loads(response.text)
-        else:
-            self.logger.info(f"POST Request Failed: {response.status_code, response.text}")
+            if response and response.status_code == 200:
+                self.logger.info(f"POST add_global_model_hash Request Successful: {response.text}")
+                return json.loads(response.text)
+            else:
+                self.logger.error(f"POST Request Failed: {response.status_code, response.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in add_global_model_hash: {str(e)}")
             return None
     
         
     def perform_aggregation(self, user_model_hashes):
-            # Placeholder for your aggregation logic
-        # Replace this with your actual aggregation logic
-        aggregated_hash = hash(''.join(user_model_hashes))
-        return aggregated_hash
+        try:
+            aggregated_hash = hash(''.join(user_model_hashes))
+            return aggregated_hash
+        except Exception as e:
+            self.logger.error(f"Error in perform_aggregation: {str(e)}")
+            return None
     
     def get_user_data(self):
-        
-        get_user = self.apiClient.get_request(get_admin_data)
+        try:
+            get_user = self.apiClient.get_request(get_admin_data)
 
-        if get_user.status_code == 200:
-            self.logger.info(f"Get Request Successful: {get_user.text}")
-            return json.loads(get_user.text)
-        else:
-            self.logger.error(f"GET Request Failed: {get_user.status_code, get_user.text}")
+            if get_user.status_code == 200:
+                self.logger.info(f"Get Request Successful: {get_user.text}")
+                return json.loads(get_user.text)
+            else:
+                self.logger.error(f"GET Request Failed: {get_user.status_code, get_user.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in get_user_data: {str(e)}")
             return None
     
     def get_latest_global_model(self):
-        
-        get_model = self.apiClient.get_request(get_global_model_hash)
+        try:
+            get_model = self.apiClient.get_request(get_global_model_hash)
 
-        if get_model.status_code == 200:
-            self.logger.info(f"Get Request Successful: {get_model.text}")
-            return json.loads(get_model.text)
-        else:
-            self.logger.error(f"GET Request Failed: {get_model.status_code, get_model.text}")
+            if get_model.status_code == 200:
+                self.logger.info(f"Get Request Successful: {get_model.text}")
+                return json.loads(get_model.text)
+            else:
+                self.logger.error(f"GET Request Failed: {get_model.status_code, get_model.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in get_latest_global_model: {str(e)}")
             return None
     
-    def post_global_model(self,glb_hash=None):
-        data = {
-        "global_model_hash": glb_hash,}
-        post_response = self.apiClient.post_request(endpoint=post_global_model_hash, data=data)
-        if post_response.status_code == 201:
-            self.logger.info(f"POST Request Successful: {post_response.text}" )
-            glb_data=json.loads(post_response.text)
-            return glb_data['global_model_hash']
-
-        else:
-            self.logger.error(f"POST Request Failed: {post_response.status_code, post_response.text}")
+    def post_global_model(self, glb_hash=None):
+        try:
+            data = {
+                "global_model_hash": glb_hash,
+            }
+            post_response = self.apiClient.post_request(endpoint=post_global_model_hash, data=data)
+            if post_response.status_code == 201:
+                self.logger.info(f"POST Request Successful: {post_response.text}")
+                glb_data = json.loads(post_response.text)
+                return glb_data['global_model_hash']
+            else:
+                self.logger.error(f"POST Request Failed: {post_response.status_code, post_response.text}")
+                return None
+        except Exception as e:
+            self.logger.error(f"Error in post_global_model: {str(e)}")
+            return None
 
     def process_user_data(self, user_data):
+        try:
+            model_hash = user_data[0]['model_hash']
+            self.model_list.append(model_hash)
+            self.logger.info(f"Send global model: {self.model_list}")
+        except Exception as e:
+            self.logger.error(f"Error in process_user_data: {str(e)}")
 
-        model_hash = user_data[0]['model_hash']
-        self.model_list.append(model_hash)
-        self.logger.info(f"Send global model: {self.model_list}")
 
-    def global_model_operations(self, ):
-        # Send all the list of hashes to aggregate and get the global model hash
-        self.global_model = self.ml_operations.aggregate_models(self.model_list)
+    def global_model_operations(self):
+        try:
+            self.global_model = self.ml_operations.aggregate_models(self.model_list)
+            self.logger.info(f"Got Global model hash: {self.global_model}")
 
-        self.logger.info(f"Got Global model hash: {self.global_model}")
-        
-        global_hash=self.post_global_model(self.global_model)
+            global_hash = self.post_global_model(self.global_model)
+            self.logger.info(self.ml_operations.send_global_model_to_others(self.mqtt_obj, global_hash))
 
-        # Sending global model hash to all workers
-        
-        self.logger.info(self.ml_operations.send_global_model_to_others(self.mqtt_obj, global_hash))
+            self.mqtt_obj.client_hash_mapping.clear()
+            self.logger.info("Clear all hash operations")
 
-        # Clearing all previous model hashes from all workers
-        self.mqtt_obj.client_hash_mapping.clear()
-        self.logger.info("Clear all hash operations")
+            latest_global_model_hash = self.get_latest_global_model()
+            self.logger.info(f"I am aggregator, here is the global model hash: {latest_global_model_hash['global_model_hash']}")
+            self.ml_operations.is_global_model_hash(latest_global_model_hash)
+        except Exception as e:
+            self.logger.error(f"Error in global_model_operations: {str(e)}")
 
-        latest_global_model_hash = self.get_latest_global_model()
-
-        self.logger.info(f"I am aggregator, here is the global model hash: {latest_global_model_hash['global_model_hash']}")
-        self.ml_operations.is_global_model_hash(latest_global_model_hash)
--- core/Role/User.py
+++ core/Role/User.py
@@ -17,91 +17,116 @@ class UserOPS:
         
         self.mqtt_obj = mqtt_comm_obj
 
-    def user_logic(self, ):
-        id=self.mqtt_obj.receive_msg('User')
-        self.mqtt_obj.send_id(role='User',id=id)
+    def user_logic(self):
+        try:
+            id = self.mqtt_obj.receive_msg('User')
+            self.mqtt_obj.send_id(role='User', id=id)
 
-        while True:
-            try :
-                self.logger.debug("Waiting")
-                time.sleep(9)
-            except KeyboardInterrupt:
-                print("Disconnected.")
-                break
+            while True:
+                try:
+                    self.logger.debug("Waiting")
+                    time.sleep(9)
+                except KeyboardInterrupt:
+                    print("Disconnected.")
+                    break
 
-        return "3"
+            return "3"
 
+            while True:
+                user_status = self.update_network_status()
+                self.send_network_status(user_status)
 
-        while(1):
-            
-    
-            user_status = self.update_network_status()
-            self.send_network_status( user_status)
+                user_status = self.update_training_status()
+                self.send_training_status(user_status)
 
-            user_status = self.update_training_status()
-            self.send_training_status(user_status)
+                hash, accuracy, loss = self.ml_operations.train_machine_learning_model()
+                self.logger.info(f"Model hash: {hash}")
 
-            hash, accuracy, loss = self.ml_operations.train_machine_learning_model()
-            self.logger.info(f"Model hash: {hash}")
+                self.send_model_to_internal_cluster(user_status, hash, accuracy, loss)
 
-            self.send_model_to_internal_cluster( user_status, hash, accuracy, loss)
+                latest_global_model_hash = self.mqtt_obj.global_model()
 
-            latest_global_model_hash = self.mqtt_obj.global_model()
+                if latest_global_model_hash:
+                    self.process_global_model_hash(latest_global_model_hash)
 
-            if latest_global_model_hash:
-                self.process_global_model_hash( latest_global_model_hash)
-
+        except Exception as e:
+            self.logger.error(f"Error in user_logic: {str(e)}")
 
     def update_network_status(self):
-        connected_status = self.apiClient.put_request(network_connected_endpoint)
+        try:
+            connected_status = self.apiClient.put_request(network_connected_endpoint)
 
-        if connected_status.status_code == 200:
-            self.logger.info(f"PUT network_status Request Successful: {connected_status.text}")
-            return json.loads(connected_status.text)
-        else:
-            self.logger.info(f"PUT Request Failed: {connected_status.status_code, connected_status.text}")
+            if connected_status.status_code == 200:
+                self.logger.info(f"PUT network_status Request Successful: {connected_status.text}")
+                return json.loads(connected_status.text)
+            else:
+                self.logger.info(f"PUT Request Failed: {connected_status.status_code, connected_status.text}")
+                return None
+
+        except Exception as e:
+            self.logger.error(f"Error in update_network_status: {str(e)}")
             return None
 
-    def send_network_status(self,  user_status):
-        if user_status:
-            message_json = json.dumps({
-                "msg": "connection_status",
-                "id": user_status['id'],
-                "network_status": user_status['network_status']
-            })
-            self.mqtt_obj.send_client_to_admin_messages(message_json)
+    def send_network_status(self, user_status):
+        try:
+            if user_status:
+                message_json = json.dumps({
+                    "msg": "connection_status",
+                    "id": user_status['id'],
+                    "network_status": user_status['network_status']
+                })
+                self.mqtt_obj.send_client_to_admin_messages(message_json)
+
+        except Exception as e:
+            self.logger.error(f"Error in send_network_status: {str(e)}")
 
     def update_training_status(self):
-        training_status = self.apiClient.put_request(toggle_training_status_endpoint)
+        try:
+            training_status = self.apiClient.put_request(toggle_training_status_endpoint)
 
-        if training_status.status_code == 200:
-            self.logger.info(f"PUT training_status Request Successful: {training_status.text}")
-            return json.loads(training_status.text)
-        else:
-            self.logger.error(f"PUT Request Failed: {training_status.status_code, training_status.text}")
+            if training_status.status_code == 200:
+                self.logger.info(f"PUT training_status Request Successful: {training_status.text}")
+                return json.loads(training_status.text)
+            else:
+                self.logger.error(f"PUT Request Failed: {training_status.status_code, training_status.text}")
+                return None
+
+        except Exception as e:
+            self.logger.error(f"Error in update_training_status: {str(e)}")
             return None
 
     def send_training_status(self, user_status):
-        if user_status:
-            message_json = json.dumps({
-                "msg": "training_status",
-                "id": user_status['id'],
-                "training_status": user_status['training_status'],
-            })
-            self.mqtt_obj.send_client_to_admin_messages(message_json)
+        try:
+            if user_status:
+                message_json = json.dumps({
+                    "msg": "training_status",
+                    "id": user_status['id'],
+                    "training_status": user_status['training_status'],
+                })
+                self.mqtt_obj.send_client_to_admin_messages(message_json)
 
-    def send_model_to_internal_cluster(self,  user_status, hash, accuracy, loss):
-        if user_status:
-            message_json = json.dumps({
-                "client_id": user_status['id'],
-                "model_hash": hash,
-                "accuracy": accuracy,
-                "loss": loss,
-            })
-            self.mqtt_obj.send_client_to_admin_model(message_json)
+        except Exception as e:
+            self.logger.error(f"Error in send_training_status: {str(e)}")
 
-    def process_global_model_hash(self,  latest_global_model_hash):
-        self.logger.info(f"I am not aggregator, got global model hash: {latest_global_model_hash}")
-        self.ml_operations.is_global_model_hash(latest_global_model_hash)
-        self.mqtt_obj.global_model_hash = None
+    def send_model_to_internal_cluster(self, user_status, hash, accuracy, loss):
+        try:
+            if user_status:
+                message_json = json.dumps({
+                    "client_id": user_status['id'],
+                    "model_hash": hash,
+                    "accuracy": accuracy,
+                    "loss": loss,
+                })
+                self.mqtt_obj.send_client_to_admin_model(message_json)
 
+        except Exception as e:
+            self.logger.error(f"Error in send_model_to_internal_cluster: {str(e)}")
+
+    def process_global_model_hash(self, latest_global_model_hash):
+        try:
+            self.logger.info(f"I am not aggregator, got global model hash: {latest_global_model_hash}")
+            self.ml_operations.is_global_model_hash(latest_global_model_hash)
+            self.mqtt_obj.global_model_hash = None
+
+        except Exception as e:
+            self.logger.error(f"Error in process_global_model_hash: {str(e)}")
