d0466dae59390ee8e4c474ded8af3dec1cb8213c
Afaan <afaan@bayes.global>
2023-12-13 23:07:40 +0530

    updated urls

--- db/bayes_app/urls.py
+++ db/bayes_app/urls.py
@@ -9,13 +9,13 @@ urlpatterns = [
     path('config/', dfladmin, name='dfladmin'),
     path('api/v1/create_training_information/', create_training_information, name='create-training-information'),
     path('api/v1/create_training_result/', create_training_result, name='create_training_result'),
-    path('api/v1/get-training-results/', get_training_results, name='get-training-results'),
+    path('api/v1/get_training_results/', get_training_results, name='get-training-results'),
 
-    path('api/v1/update-admin/', update_admin_role, name='update_admin_role'),
-    path('api/v1/update-backup-admin/', update_backup_admin_role, name='update_backup_admin_role'),
-    path('api/v1/update-user/', update_user_role, name='update_user_role'),
-    path('api/v1/update-network-status/<str:new_status>/', update_network_status, name='update_network_status'),
-    path('api/v1/toggle-training-status/', toggle_training_status, name='toggle_training_status'),
+    path('api/v1/update_admin/', update_admin_role, name='update_admin_role'),
+    path('api/v1/update_backup_admin/', update_backup_admin_role, name='update_backup_admin_role'),
+    path('api/v1/update_user/', update_user_role, name='update_user_role'),
+    path('api/v1/update_network_status/<str:new_status>/', update_network_status, name='update_network_status'),
+    path('api/v1/toggle_training-status/', toggle_training_status, name='toggle_training_status'),
     path('api/v1/create_or_update_status/', create_or_update_status, name='create_or_update_status'),
     
 
@@ -46,7 +46,7 @@ urlpatterns = [
     path('api/v1/update_model_hash/', update_model_hash, name='update_model_hash'),
     path('api/v1/get_training_info/<str:training_name>/', get_training_information_by_name, name='get_training_info'),
     path('api/v1/get_admin_data/', get_admin_data, name='get_admin_data'),
-    path('api/v1/get-track-role/', get_track_role, name='get_track_role'),
+    path('api/v1/get_track_role/', get_track_role, name='get_track_role'),
     path('api/v1/<str:metric_name>/<str:training_name>/', get_all_users_metrics, name='metrics-api-all'),
    
 ]
